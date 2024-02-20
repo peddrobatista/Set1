b5e29ba25a7e9ff47e921960edbbfb50bd863d1e
pedro-batista <peddrobatista2022@gmail.com>
2024-02-20 16:12:03 -0300

    Segundo commit...

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,13 @@ package application;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
+
+import entities.LogEntry;
 
 public class Program {
 	
@@ -14,7 +20,22 @@ public class Program {
 		String path = sc.nextLine();
 		
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			Set<LogEntry> set = new HashSet<>();
 			
+			String line = br.readLine(); // Lendo a primeira linha do arquivo
+			while (line != null) {
+				
+				String[] fields = line.split(" ");
+				String username = fields[0];
+				Date moment = Date.from(Instant.parse(fields[1]));
+				
+				set.add(new LogEntry(username, moment));
+				
+				line = br.readLine();
+				
+			}
+			
+			System.out.println("Total de usuários: " + set.size());
 		} catch (IOException e) {
 			System.out.println("Error " + e.getMessage());
 		}
